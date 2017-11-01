9991c2957b87669d519aea02ba1287ee2db6868f
lilah <lilah.willacott@gmail.com>
2017-11-01 15:42:05 +0000

    small clean up of io handling, etc.

--- deploy.js
+++ deploy.js
@@ -8,16 +8,18 @@ const handler = createHandler({
   secret: 'test'
 });
 
+const port = 7777;
 const deployments = {
   'boo-deploy': './boo-deploy.sh',
 };
 
+console.log(`Listening for deployment hooks on port ${port}`);
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
     res.end('no such location');
   });
-}).listen(7777);
+}).listen(port);
 
 handler.on('error', function (err) {
   console.error('Error:', err.message);
@@ -26,23 +28,25 @@ handler.on('error', function (err) {
 handler.on('release', function (event) {
   let repository = event.payload.repository;
   let release = event.payload.release;
-  console.log('Received a release event for %s action=%s: %s (%s)',
-    repository.name,
-    event.payload.action,
-    release.tag_name,
-    release.author.login);
+
+  console.log(
+    `Received a release event for ${respository.name}` +
+   ` ${release.tag_name} (${release.author.login})`);
+
     if (deployments.hasOwnProperty(repository.name)) {
       console.log('Found deployment scripts, running...');
+      
       exec(deployments[repository.name],
         function(err, stdout, stderr) {
+          if (err != null) {
+            console.log(`Could not execute deployment script -> ${err}`);
+            return;
+          }
 
-        let logstdio = function(description, stdio) {
-          if (stdio != null)
-            console.log(description + ': ' + stdio);
-        };
-        logstdio('output', stdout);
-        logstdio('stderr', stderr);
-        logstdio('error', err);
+          if (stdout != null && stdout != '')
+            console.log(`Output: ${stdout}`);
+          if (stderr != null && stderr != '')
+            console.log(`I/O Error: ${stderr}`)
       });
     }
 });
