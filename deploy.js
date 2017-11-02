87ccd1c797d0c9607a566baf0208aff26f02edc6
lilah <lilah.willacott@gmail.com>
2017-11-02 10:25:14 +0000

    make deployment scripts configurable outside of source control

--- deploy.js
+++ deploy.js
@@ -3,29 +3,31 @@ const http = require('http');
 const exec = require('child_process').exec;
 
 const createHandler = require('github-webhook-handler');
-const dotenv = require('dotenv');
 
-dotenv.config({path: 'deploy.env'});
-const port = process.env.SERVER_PORT;
-const secret = process.env.SECRET_KEY;
+process.env.NODE_CONFIG_DIR='./';
+const config = require('config');
+
+const serverConfig = config.get('server');
+const githubConfig = config.get('github');
+const deployConfig = config.get('deployment_scripts');
+
+const serverPath = serverConfig.get('path');
+const serverPort = serverConfig.get('port');
+const webhookSecret = githubConfig.get('webhook_secret');
 
 const handler = createHandler({
-  path: '/',
-  secret: secret
+  path: serverPath,
+  secret: webhookSecret
 });
 
-const deployments = {
-  'boo-deploy': './boo-deploy.sh',
-};
-
-console.log(`Listening for deployment hooks on port ${port}`);
+console.log(`Listening for releases at ${serverPath} on port ${serverPort}`);
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
     res.end('no such location');
   });
-}).listen(port);
+}).listen(serverPort);
 
 handler.on('error', function (err) {
   console.error('Error:', err.message);
@@ -39,10 +41,10 @@ handler.on('release', function (event) {
     `Received a release event for ${repository.name}` +
    ` ${release.tag_name} (${release.author.login})`);
 
-    if (deployments.hasOwnProperty(repository.name)) {
+    if (deployConfig.has(repository.name)) {
       console.log('Found deployment scripts, running...');
       
-      exec(deployments[repository.name],
+      exec(deployConfig.get(repository.name),
         function(err, stdout, stderr) {
           if (err != null) {
             console.log(`Could not execute deployment script -> ${err}`);
